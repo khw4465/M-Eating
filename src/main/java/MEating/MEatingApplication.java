73181b0e32c182ba366cad5c7bbcf0cdda75afa0
hyunwoo <khw4465@naver.com>
2023-12-14 01:00:12 +0900

    [CREATE]SpringBoot & JPA 프로젝트 생성

---
+++ src/main/java/MEating/MEatingApplication.java
@@ -0,0 +1,13 @@
+package MEating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MEatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MEatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MEating/MEatingApplicationTests.java
@@ -0,0 +1,13 @@
+package MEating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MEatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
