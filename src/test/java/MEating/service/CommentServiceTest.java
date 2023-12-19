850d2ff9f202d06305929d5eef183d3b506b4320
hyunwoo <khw4465@naver.com>
2023-12-20 01:52:44 +0900

    [UPDATE]댓글 테스트코드 추가 및 수정

--- src/test/java/MEating/service/CommentServiceTest.java
+++ src/test/java/MEating/service/CommentServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -72,5 +74,45 @@ public class CommentServiceTest {
         commentService.write(member, findBoard, content);
 
         fail("예외가 발생해야한다.");
+    }
+
+    @Test
+    @Rollback(false)
+    public void 게시물삭제후_댓글확인() {
+        Member member = new Member();
+        member.setName("김채원");
+        member.addRegionToMember(RegionName.Seoul);
+        em.persist(member);
+
+        Board board = Board.createBoard(member, FoodType.SUSHI, 0);
+        board.setTitle("너 내 동료가 되라");
+        board.setContent("도독도도ㅔ독");
+        Long boardId = boardService.write(member, board);
+        Board findBoard = boardService.findOne(boardId);
+
+        String content1 = "ㅋㅋㅋㅋㅋㅋ";
+        String content2 = "김도독씨";
+
+        commentService.write(member, board, content1);
+        commentService.write(member, board, content2);
+
+//
+//        assertEquals(1, member.getBoards().size());
+//
+//        for (Comment comment : commentService.findAll(findBoard)) {
+//            System.out.println("BeforeComment = " + comment);
+//        }
+
+        boardService.remove(board);
+
+        Board findBoard2 = boardService.findOne(boardId);
+        System.out.println("findBoard = " + findBoard2);
+
+        List<Comment> list2 = commentService.findAll(findBoard2);
+        System.out.println("list = " + list2.size());
+
+//        for (Comment comment : commentService.findAll(findBoard2)) {
+//            System.out.println("AfterComment = " + comment);
+//        }
     }
 }
